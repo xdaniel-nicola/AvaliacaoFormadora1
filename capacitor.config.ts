41ec26c29ee8c447e10b481a1956141d739a9981
xdaniel-nicola <daniel.nicola02@gmail.com>
2025-09-06 11:38:43 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'avaliacaoformadora1',
+  webDir: 'www'
+};
+
+export default config;
